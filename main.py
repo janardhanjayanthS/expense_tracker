e8c48acb09194c3181faa50748ff53afd32f9ed5
janardhanjayanthS <janardhanjayanth@bitcot.com>
2025-11-04 16:27:30 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+
+
+
+
+def add_expense():
+    """
+    """
+    
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    
+    while True:
+        print('Expense Tracker: ')
+        print('1. Add Expense')
+
+        choice = int(input('Enter Your choice from 1 to : '))
+
+        if choice == 1:
+            add_expense()
